6f6ac0822b2182dc529b7dffd37c0158b1439ce0
MarcAnthony-SVG <Marcanthonyrodriguez95@gmail.com>
2021-01-15 18:18:54 -0600

    Reuploading to main

--- pages/testpage.js
+++ pages/testpage.js
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from 'react';
 // import axios from 'axios';
 const TestPage = () => (
   <div>
-    <video src={require('../public/tattooing.mp4')} />
+    {/* <video src={require('../public/tattooing.mp4')} /> */}
   </div>
 )
 export default TestPage
